65a8c5037d467818b7e2cdf1de510066994e8fdc
ForkSoft90 <kd1227385@st.kobedenshi.ac.jp>
2024-07-05 15:14:56 +0900

    L06_Random  ランダム処理の確認
    2024_07_05

---
+++ L06_Random/Src/Application/Scene/GameScene/GameScene.cpp
@@ -0,0 +1,68 @@
+﻿#include "GameScene.h"
+#include"../SceneManager.h"
+
+#include "../../GameObject/Terrain/Terrain.h"
+#include "../../GameObject/Character/Character.h"
+
+#include "../../GameObject/Camera/FPSCamera/FPSCamera.h"
+#include "../../GameObject/Camera/TPSCamera/TPSCamera.h"
+#include "../../GameObject/Camera/CCTVCamera/CCTVCamera.h"
+
+void GameScene::Init()
+{
+	//===================================================================
+	// ステージ初期化
+	//===================================================================
+	std::shared_ptr<Terrain> _terrain = std::make_shared<Terrain>();
+	_terrain->Init();
+	AddObject(_terrain);
+
+	//===================================================================
+	// キャラクター初期化
+	//===================================================================
+	std::shared_ptr<Character> _character = std::make_shared<Character>();
+	_character->Init();
+	AddObject(_character);
+
+	//===================================================================
+	// カメラ初期化
+	//===================================================================
+//	std::shared_ptr<FPSCamera>		_camera = std::make_shared<FPSCamera>();
+//	std::shared_ptr<TPSCamera>		_camera = std::make_shared<TPSCamera>();
+	std::shared_ptr<CCTVCamera>		_camera = std::make_shared<CCTVCamera>();
+	_camera->Init();
+	_camera->SetTarget(_character);
+	_camera->RegistHitObject(_terrain);
+	_character->SetCamera(_camera);
+	AddObject(_camera);
+
+	//===================================================================
+	// 乱数実験場
+	//===================================================================
+	int randRes[10] = {};
+
+	// 如何にrand()が偏るか
+	srand((unsigned)time(NULL));
+	for (int i = 0; i < 100000000; i++)
+	{
+		int tmp = rand() % 10000;
+		int idx = tmp / 1000;
+
+		randRes[idx]++;
+	}
+	// 結果を出力
+	OutputDebugStringA("-----------------------------\n");
+	for(int i = 0; i < 10; i++)
+	{
+		std::stringstream ss;
+		ss << "取得した値が" << i * 1000 << "～" << (i + i) * 1000 <<
+			"の件数 : \t" << randRes[i] << " \n";
+		std::string str = ss.str();
+		OutputDebugStringA(str.c_str());
+	}
+	OutputDebugStringA("-----------------------------\n");
+}
+
+void GameScene::Event()
+{
+}
