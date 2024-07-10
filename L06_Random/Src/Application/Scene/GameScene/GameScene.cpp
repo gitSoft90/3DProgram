8825a4108442943b9922768c3fd4b01d60443c69
ForkSoft90 <kd1227385@st.kobedenshi.ac.jp>
2024-07-10 12:14:13 +0900

    L06_Random  確率計算レッスン１～３
    2024_07_10

--- L06_Random/Src/Application/Scene/GameScene/GameScene.cpp
+++ L06_Random/Src/Application/Scene/GameScene/GameScene.cpp
@@ -8,6 +8,14 @@
 #include "../../GameObject/Camera/TPSCamera/TPSCamera.h"
 #include "../../GameObject/Camera/CCTVCamera/CCTVCamera.h"
 
+// 少数第n位で四捨五入する		// メルセンヌツイスタの確認用
+void round_n(float& number, int n)
+{
+	number = number * pow(10, n - 1);
+	number = round(number);
+	number /= pow(10, n - 1);
+}
+
 void GameScene::Init()
 {
 	//===================================================================
@@ -41,26 +49,180 @@ void GameScene::Init()
 	//===================================================================
 	int randRes[10] = {};
 
-	// 如何にrand()が偏るか
-	srand((unsigned)time(NULL));
-	for (int i = 0; i < 100000000; i++)
-	{
-		int tmp = rand() % 10000;
-		int idx = tmp / 1000;
+	//// 如何にrand()が偏るか
+	//srand((unsigned)time(NULL));
+	//for (int i = 0; i < 100000000; i++)
+	//{
+	//	int tmp = rand() % 10000;
+	//	int idx = tmp / 1000;
 
-		randRes[idx]++;
+	//	randRes[idx]++;
+	//}
+	//// 結果を出力
+	//OutputDebugStringA("-----------------------------\n");
+	//for(int i = 0; i < 10; i++)
+	//{
+	//	std::stringstream ss;
+	//	ss << "取得した値が" << i * 1000 << "～" << (i + 1) * 1000 <<
+	//		"の件数 : \t" << randRes[i] << " \n";
+	//	std::string str = ss.str();
+	//	OutputDebugStringA(str.c_str());
+	//}
+	//OutputDebugStringA("-----------------------------\n");
+
+	// メルセンヌツイスタ！！！！！！！！！！！！！！
+	//for (int i = 0; i < 100000000; i++)
+	//{
+	//	int tmp = KdGetInt(0,9999);
+	//	int idx = tmp / 1000;
+
+	//	randRes[idx]++;
+	//}
+	//// 結果を出力
+	//OutputDebugStringA("-----------------------------\n");
+	//for(int i = 0; i < 10; i++)
+	//{
+	//	std::stringstream ss;
+	//	ss << "取得した値が" << i * 1000 << "～" << (i + 1) * 1000 <<
+	//		"の件数 : \t" << randRes[i] << " \n";
+	//	std::string str = ss.str();
+	//	OutputDebugStringA(str.c_str());
+	//}
+	//OutputDebugStringA("-----------------------------\n");
+
+	// レッスンその１：CカードとRカードをそれぞれ50%の確率で起動時に表示せよ！
+	/*OutputDebugStringA("-------------------------------\n");
+	std::stringstream ss;
+	int _rnd = KdGetInt(0, 1);
+	if(_rnd == 0)
+	{
+		ss << "Cカード当選！" << " \n";
 	}
-	// 結果を出力
-	OutputDebugStringA("-----------------------------\n");
-	for(int i = 0; i < 10; i++)
+	else
+	{
+		ss << "Rカード当選！" << " \n";
+	}
+	std::string str = ss.str();
+	OutputDebugStringA(str.c_str());
+	OutputDebugStringA("-------------------------------\n");*/
+
+	// レッスンその１(別解)：CカードとRカードをそれぞれ50%の確率で起動時に表示せよ！
+	/*OutputDebugStringA("-------------------------------\n");
+	int _ThusenNum		= 10000000;
+	int _TosenNum[2]    = { 0,0 };
+	for (int i = 0; i < _ThusenNum; i++)
+	{
+		int _rnd = KdGetInt(0, 1);
+		_TosenNum[_rnd]++;
+	}
+
+	float prob = 0;
+	for (int i = 0; i < 2; i++)
 	{
 		std::stringstream ss;
-		ss << "取得した値が" << i * 1000 << "～" << (i + i) * 1000 <<
-			"の件数 : \t" << randRes[i] << " \n";
+		prob = ((float)_TosenNum[i] / (float)_ThusenNum) * 100;
+		round_n(prob, 3);
+		switch (i)
+		{
+		case 0:
+			ss << "Cカード当選回数 = " << _TosenNum[0] << " " <<
+				"当選確率 = " << prob << "%" << "\n";
+			break;
+		case 1:
+			ss << "Rカード当選回数 = " << _TosenNum[1] << " " <<
+				"当選確率 = " << prob << "%" << "\n";
+		}
 		std::string str = ss.str();
 		OutputDebugStringA(str.c_str());
 	}
-	OutputDebugStringA("-----------------------------\n");
+	OutputDebugStringA("-------------------------------\n");*/
+
+	// レッスンその２：CカードとRカードをそれぞれ99.5%(Cカード) 0.5%(Rカード)の確率で起動時に表示せよ！
+	/*OutputDebugStringA("-------------------------------\n");
+	int _bunbo			= 1000;
+	int _ThusenNum		= 10000000;
+	int _TosenNum[2]	= { 0,0 };
+	for (int i = 0; i < _ThusenNum; i++)
+	{
+		int _rnd = KdGetInt(0, _bunbo - 1);
+		if (_rnd <= 4)
+		{
+			_TosenNum[1]++;
+		}
+		else
+		{
+			_TosenNum[0]++;
+		}
+	}
+
+	float prob = 0;
+	for (int i = 0; i < 2; i++)
+	{
+		std::stringstream ss;
+		prob = ((float)_TosenNum[i] / (float)_ThusenNum) * 100;
+		round_n(prob, 3);
+		switch (i)
+		{
+		case 0:
+			ss << "Cカード当選回数 = " << _TosenNum[0] << " " <<
+				"当選確率 = " << prob << "%" << "\n";
+			break;
+		case 1:
+			ss << "Rカード当選回数 = " << _TosenNum[1] << " " <<
+				"当選確率 = " << prob << "%" << "\n";
+		}
+		std::string str = ss.str();
+		OutputDebugStringA(str.c_str());
+	}
+	OutputDebugStringA("-------------------------------\n");*/
+
+	// レッスンその３：CカードとRカードとSRカードをそれぞれ34% 33% 33%の確率で起動時に表示せよ！
+	OutputDebugStringA("-------------------------------\n");
+	int _bunbo			= 1000;
+	int _ThusenNum		= 10000000;
+	int _TosenNum[3]	= { 0,0,0 };
+	for (int i = 0; i < _ThusenNum; i++)
+	{
+		int _rnd = KdGetInt(0, _bunbo - 1);
+		if (_rnd < 330)
+		{
+			_TosenNum[0]++;
+		}
+		else if (_rnd < 670)
+		{
+			_TosenNum[1]++;
+		}
+		else
+		{
+			_TosenNum[2]++;
+		}
+
+	}
+
+	float prob = 0;
+	for (int i = 0; i < 3; i++)
+	{
+		std::stringstream ss;
+		prob = ((float)_TosenNum[i] / (float)_ThusenNum) * 100;
+		round_n(prob, 3);
+		switch (i)
+		{
+		case 0:
+			ss << "Cカード当選回数 = " << _TosenNum[0] << " " <<
+				"当選確率 = " << prob << "%" << "\n";
+			break;
+		case 1:
+			ss << "Rカード当選回数 = " << _TosenNum[1] << " " <<
+				"当選確率 = " << prob << "%" << "\n";
+			break;
+		case 2:
+			ss << "SRカード当選回数 = " << _TosenNum[2] << " " <<
+				"当選確率 = " << prob << "%" << "\n";
+		}
+		std::string str = ss.str();
+		OutputDebugStringA(str.c_str());
+	}
+	OutputDebugStringA("-------------------------------\n");
 }
 
 void GameScene::Event()
