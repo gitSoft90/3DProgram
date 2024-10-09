3ab3a3dea8fd5cc6ba5c88d5d8138e9a1a483dbd
ForkSoft90 <kd1227385@st.kobedenshi.ac.jp>
2024-10-09 11:16:06 +0900

    L11_WaterShader　Waterクラスの作成
    2024_10_09 L10_OutLineShaderをコピーしたもの

---
+++ L11_WaterShader/Src/Application/GameObject/Water/Water.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+class Water : public KdGameObject
+{
+public:
+	Water() {}
+	~Water() override {}
+
+	void Update() override;
+
+	void DrawLesson() override;
+
+	void Init() override;
+
+private:
+	std::shared_ptr<KdModelWork> m_spModel = nullptr;
+
+	Math::Vector3 m_pos = Math::Vector3::Zero;
+};
